40210740176e8eed93316180875b9f288e613e5c
Aminodiin1995 <amin.hassan@d-money.dj>
2025-04-04 12:58:59 +0300

    v 1.26
    updated

--- routes/api.php
+++ routes/api.php
@@ -20,36 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Proxy routes to Cloudflare Worker
-Route::prefix('cloudflare')->group(function () {
-    // Users endpoints
-    Route::get('/users', function (Request $request) {
-        $workerUrl = env('CLOUDFLARE_WORKER_URL', 'https://cold-hall-95bc.hassanamin191.workers.dev');
-        $response = Http::get("{$workerUrl}/api/users");
-        return $response->json();
-    });
-
-    Route::get('/users/{id}', function (Request $request, $id) {
-        $workerUrl = env('CLOUDFLARE_WORKER_URL', 'https://cold-hall-95bc.hassanamin191.workers.dev');
-        $response = Http::get("{$workerUrl}/api/users/{$id}");
-        return $response->json();
-    });
-
-    Route::post('/users', function (Request $request) {
-        $workerUrl = env('CLOUDFLARE_WORKER_URL', 'https://cold-hall-95bc.hassanamin191.workers.dev');
-        $response = Http::post("{$workerUrl}/api/users", $request->all());
-        return $response->json();
-    });
-
-    Route::put('/users/{id}', function (Request $request, $id) {
-        $workerUrl = env('CLOUDFLARE_WORKER_URL', 'https://cold-hall-95bc.hassanamin191.workers.dev');
-        $response = Http::put("{$workerUrl}/api/users/{$id}", $request->all());
-        return $response->json();
-    });
-
-    Route::delete('/users/{id}', function (Request $request, $id) {
-        $workerUrl = env('CLOUDFLARE_WORKER_URL', 'https://cold-hall-95bc.hassanamin191.workers.dev');
-        $response = Http::delete("{$workerUrl}/api/users/{$id}");
-        return $response->json();
-    });
-});
+Route::get('/users', [CloudflareProxyController::class, 'getUsers']);
+Route::get('/users/{id}', [CloudflareProxyController::class, 'getUser']);
+Route::post('/users', [CloudflareProxyController::class, 'createUser']);
+Route::put('/users/{id}', [CloudflareProxyController::class, 'updateUser']);
+Route::delete('/users/{id}', [CloudflareProxyController::class, 'deleteUser']);
